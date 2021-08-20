fc4b664f3ddafa27faea97c6c17f522d61cf68b2
Adams Alves <alves.adams@gmail.com>
2021-08-20 14:42:47 -0300

    Add form login

---
+++ src/Components/Login/LoginCreate.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoginCreate = () => {
+  return <div>Login Create</div>;
+};
+
+export default LoginCreate;
---
+++ src/Components/Login/LoginPasswordReset.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LoginPasswordReset() {
+  return <div>LoginPasswordReset</div>;
+}
+
+export default LoginPasswordReset;
