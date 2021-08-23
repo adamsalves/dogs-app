d81d57aa520b3e3397995340dce5402bac43412d
Adams Alves <alves.adams@gmail.com>
2021-08-23 17:47:41 -0300

    Add user create

--- src/Components/Login/LoginCreate.js
+++ src/Components/Login/LoginCreate.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { USER_POST } from "../../api";
+import useForm from "../../Hooks/useForm";
+import { UserContext } from "../../UserContext";
+import Button from "../Forms/Button";
+import Input from "../Forms/Input";
 
 const LoginCreate = () => {
-  return <div>Login Create</div>;
+  const username = useForm();
+  const email = useForm("email");
+  const password = useForm();
+
+  const { userLogin } = React.useContext(UserContext);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
+    const response = await fetch(url, options);
+    if (response.ok) userLogin(username.value, password.value);
+  }
+
+  return (
+    <section className="animeLeft">
+      <h2 className="title">Cadastra-se</h2>
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="usuario" {...username} />
+        <Input label="E-mail" type="email" name="email" {...email} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
+  );
 };
 
 export default LoginCreate;
--- src/api.js
+++ src/api.js
@@ -32,3 +32,14 @@ export function USER_GET(token) {
     },
   };
 }
+
+export function USER_POST(body) {
+  return {
+    url: API_URL + "/api/user",
+    options: {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    },
+  };
+}
