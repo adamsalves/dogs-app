9698bbc6b5768963cfd6a47f2dfa1c97bb842389
Adams Alves <alves.adams@gmail.com>
2021-08-24 12:34:12 -0300

    Add account menu

--- src/Components/User/UserHeader.js
+++ src/Components/User/UserHeader.js
@@ -1,7 +1,34 @@
 import React from "react";
+import UserHeaderNav from "./UserHeaderNav";
+import styles from "./UserHeader.module.css";
+import { useLocation } from "react-router-dom";
 
 const UserHeader = () => {
-  return <div>Header do usuário</div>;
+  const [title, setTitle] = React.useState("");
+  const location = useLocation();
+
+  // Objeto literal que retorna o nome amigável para a rota
+  function titleName(title) {
+    const titles = {
+      "/conta": "Minha conta",
+      "/conta/estatisticas": "Estatísticas",
+      "/conta/postar": "Poste sua foto",
+      default: "Minha Conta",
+    };
+    return titles[title] || titles["default"];
+  }
+
+  React.useEffect(() => {
+    const { pathname } = location;
+    setTitle(titleName(pathname));
+  }, [location]);
+
+  return (
+    <header className={styles.header}>
+      <h1 className="title">{title}</h1>
+      <UserHeaderNav />
+    </header>
+  );
 };
 
 export default UserHeader;
