76a0b50dae18eafeba99b370ef9549ae0b9d0d75
Adams Alves <alves.adams@gmail.com>
2021-08-26 16:13:06 -0300

    Add single page for photo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
 import User from "./Components/User/User";
+import Photo from "./Components/Photo/Photo";
 import { UserStorage } from "./UserContext";
 import "./App.css";
 import ProtectedRoute from "./Helpers/ProtectedRoute";
@@ -18,6 +19,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/login/*" element={<Login />} />
           <ProtectedRoute path="/conta/*" element={<User />} />
+          <Route path="/foto/:id" element={<Photo />} />
         </Routes>
         <Footer />
       </UserStorage>
--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -1,16 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import Image from "../../Helpers/Image";
-import { UserContext } from "../../UserContext";
-import PhotoComments from "./PhotoComments";
 import styles from "./PhotoContent.module.css";
+import { Link } from "react-router-dom";
+import PhotoComments from "./PhotoComments";
+import { UserContext } from "../../UserContext";
 import PhotoDelete from "./PhotoDelete";
+import Image from "../../Helpers/Image";
 
-const PhotoContent = ({ data }) => {
+const PhotoContent = ({ data, single }) => {
   const user = React.useContext(UserContext);
   const { photo, comments } = data;
+
   return (
-    <div className={styles.photo}>
+    <div className={`${styles.photo} ${single ? styles.single : ""}`}>
       <div className={styles.img}>
         <Image src={photo.src} alt={photo.title} />
       </div>
@@ -33,7 +34,7 @@ const PhotoContent = ({ data }) => {
           </ul>
         </div>
       </div>
-      <PhotoComments id={photo.id} comments={comments} />
+      <PhotoComments single={single} id={photo.id} comments={comments} />
     </div>
   );
 };
