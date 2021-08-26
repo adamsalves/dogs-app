7d6e22c04b8e8784793871a489de997825f17515
Adams Alves <alves.adams@gmail.com>
2021-08-26 12:36:00 -0300

    Add photo delete

--- src/api.js
+++ src/api.js
@@ -90,3 +90,15 @@ export function COMMENT_POST(id, comment, token) {
     },
   };
 }
+
+export function PHOTO_DELETE(id, token) {
+  return {
+    url: `${API_URL}/api/photo/${id}`,
+    options: {
+      method: "DELETE",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    },
+  };
+}
