b74d1b2af459aec79b8ac05fc68d628875b468fc
Adams Alves <alves.adams@gmail.com>
2021-08-27 12:10:49 -0300

    Add stats page

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,13 +1,33 @@
 import React from "react";
 import Head from "../../Helpers/Head";
+import useFetch from "../../Hooks/useFetch";
+import { STATS_GET } from "../../api";
+import Loading from "../../Helpers/Loading";
+import Error from "../../Helpers/Error";
+const UserStatsGraphs = React.lazy(() => import("./UserStatsGraphs"));
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
-      Estatísticas
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const token = window.localStorage.getItem("token");
+      const { url, options } = STATS_GET(token);
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <React.Suspense fallback={<div></div>}>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </React.Suspense>
+    );
+  else return null;
 };
 
 export default UserStats;
